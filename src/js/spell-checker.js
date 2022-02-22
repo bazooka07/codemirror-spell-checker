e239413d968249ece6db523f15e19faf7b6407da
Jean-Pierre Pourrez <kazimentou@free.fr>
2022-02-22 21:17:39 +0100

    multi-languages

--- src/js/spell-checker.js
+++ src/js/spell-checker.js
@@ -31,10 +31,35 @@ function CodeMirrorSpellChecker(options) {
 	// Define the new mode
 	options.codeMirrorInstance.defineMode("spell-checker", function(config) {
 		// Load AFF/DIC data
+		var urlBase = 'https://raw.githubusercontent.com/titoBouzout/Dictionaries/master/';
+		var langs = {
+			de: 'German_de_DE',
+			de_CH: 'German_de_CH',
+			en: 'English%20(American)',
+			en_AU: 'English%20(Australian)',
+			en_CA: 'English%20(Canadian).aff',
+			en_GB: 'English%20(British)',
+			en_ZA: 'English%20(South African)',
+			es: 'Spanish',
+			fr: 'French',
+		};
+		var lg = 'en';
+		if(typeof options.language == 'string') {
+			if (options.language in langs) {
+				lg = options.language;
+			} else {
+				var parts = options.language.split('_');
+				if (parts.length == 2 && parts[0] in langs) {
+					lg = parts[0];
+				}
+			}
+		}
+
 		if(!CodeMirrorSpellChecker.aff_loading) {
 			CodeMirrorSpellChecker.aff_loading = true;
+			var affUrl = urlBase + langs[lg] + '.aff';
 			var xhr_aff = new XMLHttpRequest();
-			xhr_aff.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.aff", true);
+			xhr_aff.open('GET', affUrl, true);
 			xhr_aff.onload = function() {
 				if(xhr_aff.readyState === 4 && xhr_aff.status === 200) {
 					CodeMirrorSpellChecker.aff_data = xhr_aff.responseText;
@@ -52,8 +77,9 @@ function CodeMirrorSpellChecker(options) {
 
 		if(!CodeMirrorSpellChecker.dic_loading) {
 			CodeMirrorSpellChecker.dic_loading = true;
+			var dicUrl = urlBase + langs[lg] + '.aff';
 			var xhr_dic = new XMLHttpRequest();
-			xhr_dic.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.dic", true);
+			xhr_dic.open('GET', dicUrl, true);
 			xhr_dic.onload = function() {
 				if(xhr_dic.readyState === 4 && xhr_dic.status === 200) {
 					CodeMirrorSpellChecker.dic_data = xhr_dic.responseText;
